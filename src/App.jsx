b952ba07ce627452c657276f2f3d7efa50489eaa
Pragya Mehta <pragyamehtakai@gmail.com>
2025-06-25 18:16:41 +0530

    Developed Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,29 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+
 import './App.css'
+import Search from './Components/Search.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  
+  const [searchTerm, setSearchTerm] = useState('');
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+   <main>
+    <div className='pattern'>
+      <div className='Wrapper'>
+        <header>
+          <img src="./hero.png" alt="hero image" />
+          <h1>
+            Find Your <span className='text-gradient'>Movie</span> Without Any Hassle
+          </h1>
+        </header>
+
+        <Search searchTerm={searchTerm} setSearchTerm ={ setSearchTerm}/>
+       
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    </div>
+   </main>
     </>
   )
 }
